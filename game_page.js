2e2ef405957472c88a80231f2e5a01c0ab4f5e5a
deepakahuja184 <85532677+deepakahuja184@users.noreply.github.com>
2022-01-27 20:49:28 -0600

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,42 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_questioner").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+
+function send()
+{
+    get_number1 = document.getElementById("number1").value;
+    get_number2 = document.getElementById("number2").value;
+    console.log("get_number1 = " + get_number1);
+    console.log("get_number2 = " + get_number2);
+
+    /*length_divide_2 = Math.floor(word.length/2);
+    charAt2 = word.charAt(length_divide_2);
+    console.log(charAt2);
+
+    length_minus_1 = word.length - 1;
+    charAt3 = word.charAt(length_minus_1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt1.replace(charAt3, "_");
+    console.log(charAt3);
+
+    question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_word + input_box + check_button ;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value = "";*/
+}
